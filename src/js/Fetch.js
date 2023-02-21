f599aaf7e1b75265d33abac601eb1763fa4d50f1
ahmad-karimigorji <ahmadkarimigorji@yahoo.com>
2023-02-21 19:19:18 +0330

    change api key

--- src/js/Fetch.js
+++ src/js/Fetch.js
@@ -3,7 +3,7 @@ import LoadingAndMessageView from "./LoadingAndMessageView.js";
 export default function getData(city) {
   LoadingAndMessageView.displaySkeletonLoading();
 
-  const url = `https://api.weatherapi.com/v1/forecast.json?key=5e49d263c6ab4dec9c4134103222411&q=${city}&days=7&aqi=yes&alerts=yes`;
+  const url = `https://api.weatherapi.com/v1/forecast.json?key=03dc912cd9bc4159b77152042232102&q=${city}&days=7&aqi=yes&alerts=yes`;
 
   return axios
     .get(url)
